680c3a2e98f01ff9d22b6f242e767c020bd823c0
Samuel Stirtzel <s.stirtzel@googlemail.com>
2018-10-30 16:58:07 +0100

    Added script to install flags from region-flags repository
    No need to add flags to this repository if they're already available elsewhere
    
    Signed-off-by: Samuel Stirtzel <s.stirtzel@googlemail.com>

---
+++ flag_install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+  echo "${0} usage: $0 DESTDIR" >&2
+  exit 1
+fi
+if ! [ -e "$1" ]; then
+  echo "${0}: $1 not found" >&2
+  exit 1
+fi
+if ! [ -d "$1" ]; then
+  echo "${0}: $1 not a directory" >&2
+  exit 1
+fi
+
+# Changes name to required format "flag_<locale>.png" (where locale could be locale -> "de" or locale_country -> "en_US")
+# Example 1: flag_en.png for zera-gui_en.ts
+# Example 2: flag_en_US.png for zera-gui_en_US.ts
+# Example 3: flag_en_GB.png for zera-gui_en_GB.ts
+
+install -m 0644 ./region-flags/png/DE.png $1/flag_de.png
+install -m 0644 ./region-flags/png/US.png $1/flag_en.png
