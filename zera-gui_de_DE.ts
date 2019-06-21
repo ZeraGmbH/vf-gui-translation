624faa28e38ad517ee7c040ba8de37be24ed56be
Andreas Müller <schnitzeltony@gmail.com>
2019-06-21 17:23:56 +0200

    Change german translation for continuous measurement
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-gui_de_DE.ts
+++ zera-gui_de_DE.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de_DE">
+<TS version="2.0" language="de_DE">
 <context>
     <name>ZeraTranslation</name>
     <message>
@@ -1229,7 +1229,7 @@
     </message>
     <message>
         <source>Continuous measurement</source>
-        <translation>Laufende Messung</translation>
+        <translation>fortlaufend</translation>
     </message>
     <message>
         <source>&lt;directory name&gt;/&lt;filename&gt;</source>
