5cdc28df8e43ba2366f06b5ccb998f1c48f62fe1
Andreas Müller <schnitzeltony@gmail.com>
2024-03-26 20:31:48 +0100

    es_ES/pt_PT: Translate phases to not look too ugly
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-gui_es_ES.ts
+++ zera-gui_es_ES.ts
@@ -141,17 +141,17 @@
     <message>
         <source>Phase1</source>
         <comment>Letter or number for phase e.g euro: 1 / us: A</comment>
-        <translation type="unfinished"></translation>
+        <translation>1</translation>
     </message>
     <message>
         <source>Phase2</source>
         <comment>Letter or number for phase e.g euro: 2 / us: B</comment>
-        <translation type="unfinished"></translation>
+        <translation>2</translation>
     </message>
     <message>
         <source>Phase3</source>
         <comment>Letter or number for phase e.g euro: 3 / us: C</comment>
-        <translation type="unfinished"></translation>
+        <translation>3</translation>
     </message>
     <message>
         <source>P 1</source>
