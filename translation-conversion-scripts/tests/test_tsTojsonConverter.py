4d2317c87c292a5496e7d8d86b82168d52c68ca8
amutalik <a.mutalik@zera.de>
2025-07-31 14:29:24 +0200

    tsTojsonConverter.py, test_tsTojsonConverter.py: Follow Python's naming conventions
    Signed-off-by: amutalik <a.mutalik@zera.de>

--- translation-conversion-scripts/tests/test_tsTojsonConverter.py
+++ translation-conversion-scripts/tests/test_tsTojsonConverter.py
@@ -3,7 +3,7 @@
 import unittest
 import sys, os, shutil
 sys.path.insert(0, '..')
-from tsTojsonConverter import convertAllTsToJson
+from tsTojsonConverter import convert_all_ts_to_json
 
 #####################################################################
 #                                                                   #
@@ -14,15 +14,15 @@ from tsTojsonConverter import convertAllTsToJson
 #                                                                   #
 #####################################################################
 
-def get_testFiles():
+def get_test_files():
     return ["test-data/zera-gui_test_1.ts", "test-data/zera-gui_test_2.ts"]
 
-def getAndPrepareJsonDir():
-    global jsonDir
-    jsonDir = sys.argv[1]
-    if os.path.exists(jsonDir):
-        shutil.rmtree(jsonDir) #cleanup the directory
-    os.makedirs(jsonDir)
+def get_and_prepare_json_dir():
+    global json_dir
+    json_dir = sys.argv[1]
+    if os.path.exists(json_dir):
+        shutil.rmtree(json_dir) #cleanup the directory
+    os.makedirs(json_dir)
     # unittest.main() parses sys.argv to discover which tests to run and with what verbosity.
     # Extra unknown arguments cause errors (unittest tries to interpret them as tests).
     # so delete argument before unittest.main()
@@ -31,13 +31,13 @@ def getAndPrepareJsonDir():
 class Test_tsTojsonConverter(unittest.TestCase):
 
     def test_file_count(self):
-        convertAllTsToJson(get_testFiles(), jsonDir)
-        jsonsCount = 0
-        for entry in os.listdir(jsonDir):
-            if os.path.isfile(os.path.join(jsonDir, entry)):
-                jsonsCount += 1
-        self.assertEqual(len(get_testFiles()), jsonsCount)
+        convert_all_ts_to_json(get_test_files(), json_dir)
+        jsons_count = 0
+        for entry in os.listdir(json_dir):
+            if os.path.isfile(os.path.join(json_dir, entry)):
+                jsons_count += 1
+        self.assertEqual(len(get_test_files()), jsons_count)
 
 if __name__ == "__main__":
-    getAndPrepareJsonDir()
+    get_and_prepare_json_dir()
     unittest.main()
