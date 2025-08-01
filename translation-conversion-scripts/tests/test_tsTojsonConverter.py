687be12c91100ea3f75dac8e9a87ecd0c5777a67
amutalik <a.mutalik@zera.de>
2025-08-01 14:53:57 +0200

    test_tsTojsonConverter.py: More tests
    Signed-off-by: amutalik <a.mutalik@zera.de>

--- translation-conversion-scripts/tests/test_tsTojsonConverter.py
+++ translation-conversion-scripts/tests/test_tsTojsonConverter.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 import unittest
-import sys, os, shutil
+import sys, os, shutil, json
+import xml.etree.ElementTree as ET
 sys.path.insert(0, '..')
-from tsTojsonConverter import convert_all_ts_to_json
+from tsTojsonConverter import prepare_json_name, convert_all_ts_to_json, ts_to_json
 
 #####################################################################
 #                                                                   #
@@ -28,7 +29,20 @@ def get_and_prepare_json_dir():
     # so delete argument before unittest.main()
     del sys.argv[1]
 
+def count_xml_entries(xmlFile):
+    tree = ET.parse(xmlFile)
+    root = tree.getroot()
+    message_count = 0
+    for context in root.findall('context'):
+        for message in context.findall('message'):
+            message_count += 1
+    return message_count
+
 class Test_tsTojsonConverter(unittest.TestCase):
+
+    def test_json_name(self):
+        json_name = prepare_json_name(get_test_files()[0])
+        self.assertEqual("test_1.json", json_name)
 
     def test_file_count(self):
         convert_all_ts_to_json(get_test_files(), json_dir)
@@ -37,6 +51,15 @@ class Test_tsTojsonConverter(unittest.TestCase):
             if os.path.isfile(os.path.join(json_dir, entry)):
                 jsons_count += 1
         self.assertEqual(len(get_test_files()), jsons_count)
+
+    def test_json_content(self):
+        dumped_json_file = os.path.join(json_dir, "test_1_dumped.json")
+        ts_to_json(get_test_files()[0], dumped_json_file)
+        xml_entries = count_xml_entries(get_test_files()[0])
+        with open(dumped_json_file, 'r') as file:
+            data = json.load(file)
+            json_entries = len(data)
+        self.assertEqual(xml_entries, json_entries)
 
 if __name__ == "__main__":
     get_and_prepare_json_dir()
